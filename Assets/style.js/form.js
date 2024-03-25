a65cc60759a0839b1ee7001084afe5315a20bb11
Erica Balderas <ericab@hotmail.com>
2024-03-24 21:52:32 -0600

    adding toggle

--- Assets/style.js/form.js
+++ Assets/style.js/form.js
@@ -1,24 +1,54 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme')
+// const toggleSwitch = document.querySelector('#theme-switch input[type="checkbox"]');
+// const currentTheme = localStorage.getItem('.theme');
 
-if(currentTheme){
-  document.documentElement.setAttribute('data-theme',currentTheme);
+// let mode= 'dark';
 
-  if(currentTheme ==='dark'){
-    toggleSwitch.checked = true;
+// themeSwitcher.addEventListener('click',function(){
+// // if(currentTheme){
+// //   document.documentElement.setAttribute('dark',currentTheme);
+
+//   if(currentTheme ==='dark'){
+//     mode = 'light';
+//     container.setAttribute ('class', 'light');
+//     // toggleSwitch.checked = true;
+//   }
+//   else {
+//     mode = 'dark'
+//     container.setAttribute('class','dark')
+//   }
+// });
+
+const toggleSwitch = document.getElementById('toggleSwitch');
+const body = document.body;
+
+toggleSwitch.addEventListener('click', function() {
+    body.classList.toggle('dark-mode');
+    if(mode === 'dark') {
+    mode = 'light';
+    container.setAttribute ('class', 'light');
   }
-
-}
-
-function switchTheme(e){
-  if(e.target.checked){
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme','dark');
+  else {
+    mode = 'dark';
+    container.setAttribute('class','dark');
   }
-  else{
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-  }
-}
+});
 
-toggleSwitch.addEventListener('charge', switchTheme, false);
+
+
+
+
+
+
+// function switchTheme(e){
+//   if(e.target.checked){
+//     document.documentElement.setAttribute('data-theme', 'dark');
+//     localStorage.setItem('theme','dark');
+//   }
+//   else{
+//     document.documentElement.setAttribute('data-theme', 'light');
+//     localStorage.setItem('theme', 'light');
+//   }
+
+//   toggleSwitch.addEventListener('charge', switchTheme, false);
+//   }
+// });
