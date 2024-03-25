7afd2c1a7393c06d1c374058976c50d226673348
Erica Balderas <ericab@hotmail.com>
2024-03-25 00:01:32 -0600

    blog updates

--- Assets/style.js/blog.js
+++ Assets/style.js/blog.js
@@ -1,1 +1,15 @@
-console.log('hello');
+
+const toggleSwitch = document.getElementById('toggleSwitch');
+const body = document.body;
+
+toggleSwitch.addEventListener('click', function() {
+    body.classList.toggle('dark-mode');
+    if(mode === 'dark') {
+    mode = 'light';
+    container.setAttribute ('class', 'light');
+  }
+  else {
+    mode = 'dark';
+    container.setAttribute('class','dark');
+  }
+});
